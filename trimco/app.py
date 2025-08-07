58ca9601e10969f54d34dca3535fb1c0f4fb494a
Jessica Rehak <rehak@lbl.gov>
2025-08-07 13:40:09 -0700

    added initial coil settings

--- trimco/app.py
+++ trimco/app.py
@@ -1,9 +1,17 @@
 import ttkbootstrap as ttk
 
+from trimco.gui.coil_settings import CoilSettingsFrame
+
 class TrimcoApp(ttk.Window):
     def __init__(self):
         super().__init__(title='Trimco')
         self.protocol("WM_DELETE_WINDOW", self.quit)
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.coil_settings = CoilSettingsFrame(self)
+
+        self.coil_settings.pack(side='left')
 
     def quit(self):
         self.destroy()
---
+++ trimco/gui/coil_settings.py
@@ -0,0 +1,17 @@
+from typing import Dict
+
+import ttkbootstrap as ttk
+
+class CoilSettingsFrame(ttk.Frame):
+    def __init__(self, owner):
+        super().__init__(owner, borderwidth=1, relief=ttk.RAISED)
+        self._coil_settings: Dict[int, ttk.StringVar] = {}
+        self.create_widgets()
+        
+    def create_widgets(self):
+        for i in range(17):
+            self._coil_settings[i] = ttk.StringVar(value = '0')
+            coil_frame = ttk.Frame(self)
+            ttk.Label(coil_frame, text=f'Coil {i+1}').pack(side='left')
+            ttk.Entry(coil_frame, textvariable=self._coil_settings[i]).pack(side='left')
+            coil_frame.grid(column=int(i/4), row=int(i % 4))
