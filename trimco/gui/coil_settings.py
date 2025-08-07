b362d9a17e07b6c404542fd903ec75e85b6afeb2
Jessica Rehak <rehak@lbl.gov>
2025-08-07 13:45:02 -0700

    added padding to coil entries

--- trimco/gui/coil_settings.py
+++ trimco/gui/coil_settings.py
@@ -9,9 +9,12 @@ class CoilSettingsFrame(ttk.Frame):
         self.create_widgets()
         
     def create_widgets(self):
+        frame_padding = 5
+        entry_padding = 5
         for i in range(17):
             self._coil_settings[i] = ttk.StringVar(value = '0')
             coil_frame = ttk.Frame(self)
-            ttk.Label(coil_frame, text=f'Coil {i+1}').pack(side='left')
-            ttk.Entry(coil_frame, textvariable=self._coil_settings[i]).pack(side='left')
-            coil_frame.grid(column=int(i/4), row=int(i % 4))
+            ttk.Label(coil_frame, text=f'Coil {i+1}').pack(side='left', padx=entry_padding)
+            ttk.Entry(coil_frame, textvariable=self._coil_settings[i]).pack(side='right', 
+                                                                            padx=entry_padding)
+            coil_frame.grid(column=int(i/4), row=int(i % 4), padx=frame_padding, sticky='E')
