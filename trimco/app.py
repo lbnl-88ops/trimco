43991d6af1494984c6214952a1c9ed4110a8aba5
Jessica Rehak <rehak@lbl.gov>
2025-08-08 09:19:32 -0700

    added CoilSettingsCalculated

--- trimco/app.py
+++ trimco/app.py
@@ -1,7 +1,7 @@
 import ttkbootstrap as ttk
 
 from .coordinator import Coordinator
-from trimco.gui.coil_settings import CoilSettingsFrame
+from trimco.gui.coil_settings import CoilSettingsFrame, CoilSettingsCalculatedFrame
 from trimco.gui.plot import PlotFrame
 
 class TrimcoApp(ttk.Window):
@@ -13,9 +13,11 @@ class TrimcoApp(ttk.Window):
     def create_widgets(self):
         self.coil_settings = CoilSettingsFrame(self)
         self.plot = PlotFrame(self)
+        self.coil_settings_calculated = CoilSettingsCalculatedFrame(self)
 
         self.coil_settings.pack(side='top')
         self.plot.pack(side='top')
+        self.coil_settings_calculated.pack(side='top')
         self.coordinator = Coordinator([self.plot, self.coil_settings])
 
     def quit(self):
--- trimco/gui/coil_settings.py
+++ trimco/gui/coil_settings.py
@@ -3,13 +3,13 @@ from typing import Dict
 import ttkbootstrap as ttk
 
 class CoilSettingsFrame(ttk.Frame):
-    def __init__(self, owner):
+    def __init__(self, owner, include_check_boxes = False):
         super().__init__(owner, borderwidth=1, relief=ttk.RAISED)
         self.coil_settings: Dict[int, ttk.StringVar] = {}
         self.main_current = ttk.StringVar(value = '0')
-        self.create_widgets()
+        self.create_widgets(include_check_boxes)
         
-    def create_widgets(self):
+    def create_widgets(self, include_check_boxes):
         frame_padding = 5
         entry_padding = 5
         main_current_frame = ttk.Frame(self)
@@ -17,12 +17,20 @@ class CoilSettingsFrame(ttk.Frame):
         self.entMainCurrent = ttk.Entry(main_current_frame, textvariable=self.main_current)
         self.entMainCurrent.pack(side='right', padx=entry_padding)
         self.trim_coil_entries = {}
+        self.trim_coil_checkboxes = {}
         main_current_frame.grid(column = 0, row = 0)
         for i in range(17):
             self.coil_settings[i] = ttk.StringVar(value = '0')
             coil_frame = ttk.Frame(self)
+            if include_check_boxes:
+                self.trim_coil_checkboxes[i] = ttk.BooleanVar(value=False)
+                ttk.Checkbutton(coil_frame, variable=self.trim_coil_checkboxes[i]).pack(side='left')
             ttk.Label(coil_frame, text=f'Coil {i+1}').pack(side='left', padx=entry_padding)
             coil_entry = ttk.Entry(coil_frame, textvariable=self.coil_settings[i])
             coil_entry.pack(side='right', padx=entry_padding)
             self.trim_coil_entries[i] = coil_entry
             coil_frame.grid(column=int(i/4), row=int(i % 4) + 1, padx=frame_padding, sticky='E')
+
+class CoilSettingsCalculatedFrame(CoilSettingsFrame):
+    def __init__(self, owner):
+        super().__init__(owner, include_check_boxes=True)
