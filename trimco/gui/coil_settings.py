8c27c60484009f159b19aa7e1fd8b6d249a74913
Jessica Rehak <rehak@lbl.gov>
2025-08-11 12:07:53 -0700

    restructured coil settings

--- trimco/gui/coil_settings.py
+++ trimco/gui/coil_settings.py
@@ -5,6 +5,7 @@ import ttkbootstrap as ttk
 class CoilSettingsFrame(ttk.Frame):
     def __init__(self, owner, is_calculated = False):
         super().__init__(owner, borderwidth=1, relief=ttk.RAISED)
+        self.trim_coil_entries = {}
         self.coil_settings: Dict[int, ttk.StringVar] = {}
         self.main_current = ttk.StringVar(value = '1000')
         self.create_widgets(is_calculated)
@@ -12,22 +13,20 @@ class CoilSettingsFrame(ttk.Frame):
     def create_widgets(self, is_calculated):
         frame_padding = 5
         entry_padding = 5
+
         if not is_calculated:
             main_current_frame = ttk.Frame(self)
             ttk.Label(main_current_frame, text='Main').pack(side='left', padx=entry_padding)
             self.entMainCurrent = ttk.Entry(main_current_frame, textvariable=self.main_current)
             self.entMainCurrent.pack(side='right', padx=entry_padding)
             main_current_frame.grid(column = 0, row = 0)
-        self.trim_coil_entries = {}
-        self.use_trim_coil = {}
-        self.trim_coil_checkboxes = {}
+
+        self.coil_frames = []
         for i in range(17):
             self.coil_settings[i] = ttk.StringVar(value = '0')
-            coil_frame = ttk.Frame(self)
-            if is_calculated:
-                self.use_trim_coil[i] = ttk.BooleanVar(value=False)
-                self.trim_coil_checkboxes[i] = ttk.Checkbutton(coil_frame, variable=self.use_trim_coil[i])
-                self.trim_coil_checkboxes[i].pack(side='left')
+            self.coil_frames.append(ttk.Frame(self))
+            coil_frame = self.coil_frames[-1]
+
             ttk.Label(coil_frame, text=f'Coil {i+1}').pack(side='left', padx=entry_padding)
             coil_entry = ttk.Entry(coil_frame, textvariable=self.coil_settings[i],
                                    state='normal' if not is_calculated else 'disable')
@@ -37,4 +36,14 @@ class CoilSettingsFrame(ttk.Frame):
 
 class CoilSettingsCalculatedFrame(CoilSettingsFrame):
     def __init__(self, owner):
+        self.use_trim_coil = {}
+        self.trim_coil_checkboxes = {}
         super().__init__(owner, is_calculated=True)
+
+    def create_widgets(self, is_calculated):
+        super().create_widgets(is_calculated)
+        for i, coil_frame in enumerate(self.coil_frames):
+            self.use_trim_coil[i] = ttk.BooleanVar(value=False)
+            self.trim_coil_checkboxes[i] = ttk.Checkbutton(coil_frame, variable=self.use_trim_coil[i])
+            self.trim_coil_checkboxes[i].pack(side='left')
+
